89b126427ce101941b47f0b3d25c35b738414381
alsomedroo <dhruvattek@gmail.com>
2025-04-07 09:24:57 +0530

    backend ready

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
-const express = require("express");
+const express = require("express")
+const router = express.Router()
+const rootRouter = require("./routes/index")
+const cors = require("cors")
 
 
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1",rootRouter)
+
+module.exports = router
+
+app.listen(3000);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./Signup";
+import Signin from "./Signin";
+import Dashboard from "./Dashboard";
+import SendMoney from "./SendMoney";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
